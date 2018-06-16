7568d69c1c84b6ddf9aa6121d1fba4722cdc821d
fiad <andrea.fiadone@purplenetwork.it>
2018-06-16 13:13:55 +0200

    Setup optimizations

--- app/Helper/SetupManager.php
+++ app/Helper/SetupManager.php
@@ -2,8 +2,6 @@
 
 namespace Wordrobe\Helper;
 
-use Composer\Script\Event;
-use Symfony\Component\Process\Process;
 use Wordrobe\Builder\ConfigBuilder;
 use Wordrobe\Builder\ThemeBuilder;
 
@@ -27,17 +25,5 @@ class SetupManager
     if (Dialog::getConfirmation('Do you want to add a new theme right now?', true, 'yellow')) {
       ThemeBuilder::startWizard();
     }
-  }
-
-  /**
-   * Handles library installation from script
-   * @param Event $event
-   */
-  public static function installFromScript(Event $event)
-  {
-    $process = new Process('php ' . __DIR__ . '/../../bin/wordrobe init');
-    $process->run(function ($type, $buffer) use ($event) {
-      $event->getIO()->write($buffer, false);
-    });
   }
 }
